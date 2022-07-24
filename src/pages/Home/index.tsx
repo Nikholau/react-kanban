4b3a435c09d7a00e73eff39b008cd136505fd4c1
Nikholau <nikholasyan12@gmail.com>
2022-07-24 20:45:52 -0300

    drag in drop

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,56 +1,92 @@
-import Board from '@asseinfo/react-kanban';
+import Board, { moveCard } from '@asseinfo/react-kanban';
 import '@asseinfo/react-kanban/dist/styles.css';
-
-import blank from '../../assets/blank.png';
+import { useState } from 'react';
 
 import './styles.scss';
 
 export const Home = () => {
-  const board = {
+  let initialBoard = {
+    counter: 9,
     columns: [
       {
-        id: 1,      
-        title: 'To Do',
+        id: 1,
+        title: "📝  To Do",
         cards: [
           {
-            id: 1,
-            title: 'Add card',
-            description: 'Add capability to add a card in a column'
+            id: 1.1,
+            title: "Documentar padrões mobile",
+            description: "Tag1"
           },
+          {
+            id: 1.2,
+            title: "Ajustes fluxo de compra",
+            description: "Tag1"
+          },
+          {
+            id: 1.3,
+            title: "Banners da home",
+            description: "Tag1"
+          },
+          {
+            id: 1.4,
+            title: "Template de e-mail marketing",
+            description: "Tag1"
+          }
         ]
       },
       {
         id: 2,
-        title: 'In Progress',
+        title: "💻  In Progress",
         cards: [
           {
-            id: 2,
-            title: 'Drag-n-drop support',
-            description: 'Move a card between the columns'
-          },
+            id: 2.1,
+            title: "Wireframe das telas",
+            description: "Tag2"
+          }
         ]
       },
       {
         id: 3,
-        title: 'Done',
+        title: "🚀  Done",
         cards: [
           {
-            id: 3,
-            title: 'Drag-n-drop support',
-            description: 'Move a card between the columns'
+            id: 3.1,
+            title: "Implementação do blog",
+            description: "Tag3"
           },
+          {
+            id: 3.2,
+            title: "Análise de métricas",
+            description: "Tag3"
+          },
+          {
+            id: 3.3,
+            title: "UX Review",
+            description: "Tag3"
+          }
         ]
       }
     ]
+  };
+
+  const [board, setBoard] = useState(initialBoard);
+
+  function handleCardMove(_card: any, source: any, destination: any) {
+    const updatedBoard = moveCard(board, source, destination);
+    setBoard(updatedBoard);
   }
+ 
+
   return(
     <div className="container">
       <h1 className='title'>Kanban do projeto</h1>
       <div className="kanbanContainer">
-      <Board initialBoard={board} />
-      </div>
-      <div className='image'>
-      <img src={blank} alt="decoration" />
+        <Board 
+          onCardDragEnd={handleCardMove}
+          disableColumnDrag
+        > 
+          {board}
+        </Board>
       </div>
     </div>
   );
