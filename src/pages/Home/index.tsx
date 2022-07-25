9a7a4cde8959c7c4444f094caf1ea59f47645fad
Nikholau <nikholasyan12@gmail.com>
2022-07-25 12:38:23 -0300

    finish

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,74 +1,28 @@
 import Board, { moveCard } from '@lourenci/react-kanban';
 import '@asseinfo/react-kanban/dist/styles.css';
 import { useState } from 'react';
-
 import './styles.scss';
+import { initialBoard } from '../../helpers/data';
+
+interface ICard {
+  id: number;
+  title: string;
+  description?: string;
+}
+
+interface IColumns {
+  id: number;
+  title: string;
+  cards: ICard[];
+}
+
+export interface IBoard {
+  counter: number;
+  columns: IColumns[];
+}
 
 export const Home = () => {
-  let initialBoard = {
-    counter: 9,
-    columns: [
-      {
-        id: 1,
-        title: "📝  To Do",
-        cards: [
-          {
-            id: 1.1,
-            title: "Documentar padrões mobile",
-            description: "Tag1"
-          },
-          {
-            id: 1.2,
-            title: "Ajustes fluxo de compra",
-            description: "Tag1"
-          },
-          {
-            id: 1.3,
-            title: "Banners da home",
-            description: "Tag1"
-          },
-          {
-            id: 1.4,
-            title: "Template de e-mail marketing",
-            description: "Tag1"
-          }
-        ]
-      },
-      {
-        id: 2,
-        title: "💻  In Progress",
-        cards: [
-          {
-            id: 2.1,
-            title: "Wireframe das telas",
-            description: "Tag2"
-          }
-        ]
-      },
-      {
-        id: 3,
-        title: "🚀  Done",
-        cards: [
-          {
-            id: 3.1,
-            title: "Implementação do blog",
-            description: "Tag3"
-          },
-          {
-            id: 3.2,
-            title: "Análise de métricas",
-            description: "Tag3"
-          },
-          {
-            id: 3.3,
-            title: "UX Review",
-            description: "Tag3"
-          }
-        ]
-      }
-    ]
-  };
-  const [board, setBoard] = useState(initialBoard);
+  const [board, setBoard] = useState<IBoard>(initialBoard);
 
   function onCardNew(newCard: any) {
     const newCardLocal = { id: initialBoard.counter + 1, ...newCard };
@@ -77,7 +31,7 @@ export const Home = () => {
     return newCardLocal;
   }
 
-  function handleCardMove(_card: any, source: any, destination: any) {
+  function handleCardMove(_card: ICard, source: number, destination: number) {
     const updatedBoard = moveCard(board, source, destination);
     setBoard(updatedBoard);
   }
@@ -91,9 +45,12 @@ export const Home = () => {
           onCardDragEnd={handleCardMove}
           initialBoard={board} 
           allowRenameColumn
+          allowRemoveCard
           allowAddCard={{ on: "Bottom" }}
           onNewCardConfirm={onCardNew}
           onCardNew={console.log} 
+          onCardRemove={console.log}
+          
           />
       </div>
     </div>
